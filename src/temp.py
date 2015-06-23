2da3a96bc330646fab11f88e5c0486ef1951e92c
Arman Sadybekov <sadybeko@usc.edu>
2015-06-23 06:51:18 -0700

    fixed syntax

--- src/temp.py
+++ src/temp.py
@@ -7,7 +7,7 @@ def f_to_k(temp):
     converted = (temp - 32) * 5.0/9.0 + 273.15
     return converted
 
-def k_to_f(temp)
+def k_to_f(temp):
     converted = (temp-273.15)*9.0/5.0
 
 print f_to_k(32)
