84ad728856cb3088ba090afe097efb8a6c2ac84b
Arman Sadybekov <sadybeko@usc.edu>
2015-06-23 06:49:36 -0700

    added k_to_f and cleaned code a little

--- src/temp.py
+++ src/temp.py
@@ -4,7 +4,10 @@
 """
 
 def f_to_k(temp):
-    converted = ((temp - 32) * 5.0/9.0) + 273.15
+    converted = (temp - 32) * 5.0/9.0 + 273.15
     return converted
+
+def k_to_f(temp)
+    converted = (temp-273.15)*9.0/5.0
 
 print f_to_k(32)
